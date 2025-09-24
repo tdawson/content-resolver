c4c7df72ab26c9b2dcc4cb7de506cd2ffc7ac87f
Troy Dawson <tdawson@redhat.com>
2025-09-24 06:32:10 -0700

    Revert "Merge pull request #83 from tdawson/buildroot-extras"
    This reverts commit 7280ef9a24a80c844e731d45ada4030d337c847e, reversing
    changes made to 9dd5446722f31b941273aaf740faa7e606857494.

--- content_resolver/analyzer.py
+++ content_resolver/analyzer.py
@@ -2279,8 +2279,7 @@ class Analyzer():
         for view_conf_id in self.configs["views"]:
             view_conf = self.configs["views"][view_conf_id]
 
-            #if view_conf["type"] == "compose":
-            if True:
+            if view_conf["type"] == "compose":
                 if view_conf["buildroot_strategy"] == "root_logs":
                     for arch in view_conf["architectures"]:
                         self._populate_buildroot_with_view_srpms(view_conf, arch)
@@ -2488,8 +2487,7 @@ class Analyzer():
         for view_conf_id in self.configs["views"]:
             view_conf = self.configs["views"][view_conf_id]
 
-            #if view_conf["type"] == "compose":
-            if True:
+            if view_conf["type"] == "compose":
                 if view_conf["buildroot_strategy"] == "root_logs":
                     for arch in view_conf["architectures"]:
                         self._add_buildroot_to_view(view_conf, arch)
@@ -2753,8 +2751,7 @@ class Analyzer():
                 view_all_arches["id"] = view_conf_id
                 view_all_arches["has_buildroot"] = False
 
-                #if view_conf["type"] == "compose":
-                if True:
+                if view_conf["type"] == "compose":
                     if view_conf["buildroot_strategy"] == "root_logs":
                         view_all_arches["has_buildroot"] = True
                 else:
